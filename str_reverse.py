3cbf108d9d5dca98f473f7d41fb31b01f09769fa
Sammy Kimaiyo <skimaiyo@ymail.com>
2016-01-26 15:06:04 +0300

    add new reverse function

--- str_reverse.py
+++ str_reverse.py
@@ -1,8 +1,48 @@
-def reverse(s) :
+def reverse(s):
+	"""
+	 returns the reverse of a stting 
+	 one statement uses list comprehension
+	 """
+	return s[:: -1]
+
+
+
+def reversex(s):
+	"""
+    reverse a string using a loop
+    """
+    # makes  list from the string; because they are immutable 
+ 	s = list(s) 
+ 	# does a floor division (discards remainder) to reverse a string 
+ 	# you need to do this to avoid reversal of the reverse 
+ 	for i in range(len(s) // 2) : 
+ 		temp = s[i]
+ 		s[i] = s[len(s) - i - 1]
+ 		s[len(s) - i - 1] = temp
+ 	# makes the string from the list after reversal 
+ 	return "".join(s) 
+
+
+def reversey(s):
+	"""
+	a reversal without a temporary variable version
+	"""
+	s = list(s) 
+ 	for i in range(len(s) // 2) :
+ 		# swap to avoid using a temporary variable
+		s[i], s[len(s) - i - 1]	= s[len(s) - i - 1], s[i] 
+ 	return "".join(s)
+	
+def reversez(s):
+	"""
+	yet another way to swap
+	"""
 	pass
 
-	
+
+
 # examples
 print reverse("andela")
-print reverse("hello")
+print reversex("hello")
+print reversey("hello world")
 
